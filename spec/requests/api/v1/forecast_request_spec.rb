6202899a1a800ff8487b008d6c8ba8cdf91dc9cc
Alex Latham <alexlthm@gmail.com>
2020-06-07 16:52:06 -0600

    Add forecast spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Visitors", type: :request do
-  xit "can request forecast for a city" do
+  it "can request forecast for a city" do
     get api_v1_forecast_path(params: { location: "denver,co" })
 
     expect(response).to be_successful
--- spec/services/google_maps_services_spec.rb
+++ spec/services/google_maps_services_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe GoogleMapsServices do
     VCR.use_cassette("denver geocoding") do
       location = "denver,co"
 
-      denver_geocoding = GoogleMapsServices.new.get_geocoding(location)
+      geocoding_json = GoogleMapsServices.new.get_geocoding(location)
 
-      expect(denver_geocoding[:status]).to eq("OK")
-      expect(denver_geocoding[:results][0][:formatted_address]).to eq("Denver, CO, USA")
-      expect(denver_geocoding[:results][0][:geometry][:location]).to eq({ lat: 39.7392358, lng: -104.990251 })
+      expect(geocoding_json[:status]).to eq("OK")
+      expect(geocoding_json[:results][0][:formatted_address]).to eq("Denver, CO, USA")
+      expect(geocoding_json[:results][0][:geometry][:location]).to eq({ lat: 39.7392358, lng: -104.990251 })
     end
   end
 
   it "can retrieve geocoding for Brussels" do
     VCR.use_cassette("brussels geocoding") do
       location = "brussels,be"
-      
-      brussels_geocoding = GoogleMapsServices.new.get_geocoding(location)
 
-      expect(brussels_geocoding[:status]).to eq("OK")
-      expect(brussels_geocoding[:results][0][:formatted_address]).to eq("Brussels, Belgium")
-      expect(brussels_geocoding[:results][0][:geometry][:location]).to eq({ lat: 50.8503396, lng: 4.3517103 })
+      geocoding_json = GoogleMapsServices.new.get_geocoding(location)
+
+      expect(geocoding_json[:status]).to eq("OK")
+      expect(geocoding_json[:results][0][:formatted_address]).to eq("Brussels, Belgium")
+      expect(geocoding_json[:results][0][:geometry][:location]).to eq({ lat: 50.8503396, lng: 4.3517103 })
     end
   end
 end
