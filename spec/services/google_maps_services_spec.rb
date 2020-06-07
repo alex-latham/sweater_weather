973ba171404daacbdfc2dad7f6b8acbaabf9ce34
Alex Latham <alexlthm@gmail.com>
2020-06-07 10:20:57 -0600

    Add non-US city for testing geocode

--- spec/services/google_maps_services_spec.rb
+++ spec/services/google_maps_services_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Visitor" do
-  it "can retrieve coordinates for a city" do
+  it "can retrieve coordinates for an American city" do
     VCR.use_cassette("denver geocode") do
       location = "denver,co"
       json = GoogleMapsServices.new.get_coordinates(location)
@@ -9,6 +9,17 @@ RSpec.describe "Visitor" do
       expect(json[:status]).to eq("OK")
       expect(json[:results][0][:formatted_address]).to eq("Denver, CO, USA")
       expect(json[:results][0][:geometry][:location]).to eq({ lat: 39.7392358, lng: -104.990251 })
+    end
+  end
+
+  it "can retrieve coordinates for a Belgian city" do
+    VCR.use_cassette("brussels geocode") do
+      location = "brussels,be"
+      json = GoogleMapsServices.new.get_coordinates(location)
+
+      expect(json[:status]).to eq("OK")
+      expect(json[:results][0][:formatted_address]).to eq("Brussels, Belgium")
+      expect(json[:results][0][:geometry][:location]).to eq({ lat: 50.8503396, lng: 4.3517103 })
     end
   end
 end
