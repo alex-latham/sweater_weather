f4776b386bcf76486b4974de113a98119f7c4ba7
Alex Latham <alexlthm@gmail.com>
2020-06-08 11:44:24 -0600

    Add foodie spec

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -10,16 +10,17 @@ class Foodie
   end
 
   def self.search(foodie_params)
-    end_location = foodie_params[:end]
     directions = Directions.with_origin_destination(foodie_params[:start], foodie_params[:end])
     destination_geocoding = Geocoding.from_location_name(foodie_params[:end])
     forecast = Forecast.from_geocoding(destination_geocoding)
     restaurant = Restaurant.by_geocoding_and_cuisines(destination_geocoding, foodie_params[:search])
-    foodie_info = prepare_foodie_info(end_location, directions, forecast, restaurant)
+    foodie_info = self.prepare_foodie_info(foodie_params[:end], directions, forecast, restaurant)
     new(foodie_info)
   end
 
-  def prepare_foodie_info(end_location, directions, forecast, restaurant)
+  private
+
+  def self.prepare_foodie_info(end_location, directions, forecast, restaurant)
     { end_location: end_location,
       travel_time: directions.travel_time,
       forecast: {
