db4c27e6739588ccc13a4960784754367229060f
Alex Latham <alexlthm@gmail.com>
2020-06-09 15:18:51 -0600

    Test geocoding responses for different places

--- spec/services/google_maps_services_spec.rb
+++ spec/services/google_maps_services_spec.rb
@@ -1,14 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe GoogleMapsServices do
-  it 'can retrieve location json for a city' do
+  it 'can retrieve location json for denver' do
     VCR.use_cassette('denver geocoding json') do
       location_json = GoogleMapsServices.new.get_location('denver,co')
 
       expect(location_json[:status]).to eq('OK')
-      expect(location_json[:results][0]).to have_key(:formatted_address)
-      expect(location_json[:results][0][:geometry][:location]).to have_key(:lat)
-      expect(location_json[:results][0][:geometry][:location]).to have_key(:lng)
+      expect(location_json[:results][0][:address_components][0][:short_name]).to eq('Denver')
+      expect(location_json[:results][0][:address_components][0][:types]).to eq(['locality', 'political'])
+      expect(location_json[:results][0][:address_components][2][:short_name]).to eq('CO')
+      expect(location_json[:results][0][:address_components][2][:types]).to eq(['administrative_area_level_1', 'political'])
+      expect(location_json[:results][0][:address_components][3][:long_name]).to eq('United States')
+      expect(location_json[:results][0][:address_components][3][:types]).to eq(['country', 'political'])
+      expect(location_json[:results][0][:formatted_address]).to eq('Denver, CO, USA')
+      expect(location_json[:results][0][:geometry][:location][:lat]).to eq(39.7392358)
+      expect(location_json[:results][0][:geometry][:location][:lng]).to eq(-104.990251)
+    end
+  end
+
+  it 'can retrieve location json for paris' do
+    VCR.use_cassette('paris geocoding json') do
+      location_json = GoogleMapsServices.new.get_location('paris,fr')
+
+      expect(location_json[:status]).to eq('OK')
+      expect(location_json[:results][0][:address_components][0][:short_name]).to eq('Paris')
+      expect(location_json[:results][0][:address_components][0][:types]).to eq(['locality', 'political'])
+      expect(location_json[:results][0][:address_components][2][:short_name]).to eq('IDF')
+      expect(location_json[:results][0][:address_components][2][:types]).to eq(['administrative_area_level_1', 'political'])
+      expect(location_json[:results][0][:address_components][3][:long_name]).to eq('France')
+      expect(location_json[:results][0][:address_components][3][:types]).to eq(['country', 'political'])
+      expect(location_json[:results][0][:formatted_address]).to eq('Paris, France')
+      expect(location_json[:results][0][:geometry][:location][:lat]).to eq(48.856614)
+      expect(location_json[:results][0][:geometry][:location][:lng]).to eq(2.3522219)
+    end
+  end
+
+  it 'can retrieve location json for paris' do
+    VCR.use_cassette('tokyo geocoding json') do
+      location_json = GoogleMapsServices.new.get_location('tokyo,jp')
+
+      expect(location_json[:status]).to eq('OK')
+      expect(location_json[:results][0][:address_components][0][:short_name]).to eq('Tokyo')
+      expect(location_json[:results][0][:address_components][0][:types]).to eq(['administrative_area_level_1', 'political'])
+      expect(location_json[:results][0][:address_components][1][:long_name]).to eq('Japan')
+      expect(location_json[:results][0][:address_components][1][:types]).to eq(['country', 'political'])
+      expect(location_json[:results][0][:formatted_address]).to eq('Tokyo, Japan')
+      expect(location_json[:results][0][:geometry][:location][:lat]).to eq(35.6761919)
+      expect(location_json[:results][0][:geometry][:location][:lng]).to eq(139.6503106)
+    end
+  end
+
+  it 'can retrieve location json for washington d.c.' do
+    VCR.use_cassette('washington d.c. geocoding json') do
+      location_json = GoogleMapsServices.new.get_location('dc,dc')
+
+      expect(location_json[:status]).to eq('OK')
+      expect(location_json[:results][0][:address_components][0][:short_name]).to eq('Washington')
+      expect(location_json[:results][0][:address_components][0][:types]).to eq(['locality', 'political'])
+      expect(location_json[:results][0][:address_components][2][:short_name]).to eq('DC')
+      expect(location_json[:results][0][:address_components][2][:types]).to eq(['administrative_area_level_1', 'political'])
+      expect(location_json[:results][0][:address_components][3][:long_name]).to eq('United States')
+      expect(location_json[:results][0][:address_components][3][:types]).to eq(['country', 'political'])
+      expect(location_json[:results][0][:formatted_address]).to eq('Washington, DC, USA')
+      expect(location_json[:results][0][:geometry][:location][:lat]).to eq(38.9071923)
+      expect(location_json[:results][0][:geometry][:location][:lng]).to eq(-77.0368707)
     end
   end
 end
