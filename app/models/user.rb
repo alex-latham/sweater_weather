0bf3d9a5b96981dbe481545251dd78af3ffe1db5
Alex Latham <alexlthm@gmail.com>
2020-06-09 10:31:43 -0600

    Create sessions controller, change user model expectations regarding password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, :password_digest, :api_key, presence: true
+  validates :email, :password, :api_key, presence: true
   validates :email, uniqueness: true
   validates :api_key, uniqueness: true
 end
